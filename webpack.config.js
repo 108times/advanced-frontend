a4b38863a47ca4c1149cd4ae631a2afa6df5ddb0
Amir Kadenov <amirkadenov@gmail.com>
2023-06-06 23:35:00 +0600

    build: update webpack config - add html and progress plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
 const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
   mode: 'production',
   entry: path.resolve(__dirname, 'src', 'index.js'),
@@ -7,4 +10,9 @@ module.exports = {
     filename: 'output.js',
     path: path.resolve(__dirname, 'build'),
   },
+
+  plugins: [
+    new webpack.ProgressPlugin(),
+    new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'src') }),
+  ],
 };
