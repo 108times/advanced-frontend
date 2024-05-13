06b88c01e66c2845b17cb98f7259709bf7d7313e
Amirali <kadenov.a@mybuh.kz>
2024-05-13 19:13:49 +0500

    add: basic routing

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,25 @@
 import { Counter } from "./counter";
 import "./index.scss";
-
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { AboutPage } from "./pages/about-page/AboutPage";
+import { MainPage } from "./pages/main-page/MainPage";
+import { Link } from "react-router-dom";
 export const App = () => {
   return (
     <div className="app">
-      <Counter />
+      <BrowserRouter>
+        <nav style={{ display: "flex", gap: "16px" }}>
+          <Link to="/">Главная</Link>
+          <Link to="/about">О нас</Link>
+        </nav>
+
+        <hr />
+
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/about" element={<AboutPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 };
