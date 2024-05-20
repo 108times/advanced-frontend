50c44f46a7489b86782ace9372a49b8e4807214c
Amirali <kadenov.a@mybuh.kz>
2024-05-20 18:31:54 +0500

    add: theme logic

---
+++ src/theme/ThemeProvider.tsx
@@ -0,0 +1,27 @@
+import { FC, ReactNode, useMemo, useState } from "react";
+import { LOCAL_STORAGE_THEME_KEY, Theme, ThemeContext } from "./theme-context";
+
+type Props = {
+  children: ReactNode;
+};
+
+export const ThemeProvider: FC<Props> = ({ children }) => {
+  const [theme, setTheme] = useState<Theme>(defaultTheme);
+
+  const defaultValue = useMemo(
+    () => ({
+      theme,
+      setTheme,
+    }),
+    [theme]
+  );
+
+  return (
+    <ThemeContext.Provider value={defaultValue}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+const defaultTheme =
+  (localStorage.getItem(LOCAL_STORAGE_THEME_KEY) as Theme) ?? Theme.LIGHT;
