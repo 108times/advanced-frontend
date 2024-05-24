dbc5427c43c4483be6c5eba2ff3c12a49b89400b
Amirali <amirkadenov@gmail.com>
2024-05-24 18:47:18 +0500

    change: classNames - change implementation

--- src/shared/lib/class-names/classNames.ts
+++ src/shared/lib/class-names/classNames.ts
@@ -1,13 +1,23 @@
 type Mods = Record<string, boolean | string>;
 
-export function classNames(
-  cls: string,
-  additional: string[],
-  mods?: Mods
-): string {
-  return [
-    cls,
-    ...additional,
-    ...(mods ? Object.keys(mods).filter((key) => mods[key]) : []),
-  ].join(" ");
+type Value = false | undefined | 0 | null | string;
+
+type ClassName = Mods | Value;
+
+export function classNames(...values: ClassName[]): string {
+  const result: string[] = [];
+
+  values.forEach((item) => {
+    if (typeof item === "object") {
+      result.push(...parseMods(item));
+
+      return;
+    }
+
+    if (item) result.push(item);
+  });
+
+  return result.join(" ");
 }
+
+const parseMods = (mods: Mods) => Object.keys(mods).filter((key) => mods[key]);
