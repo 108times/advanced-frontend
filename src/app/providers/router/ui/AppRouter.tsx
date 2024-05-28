2009e940b4225bc32bfb3443d45d27eeb74ba9fa
Amirali <kadenov.a@mybuh.kz>
2024-05-28 20:03:05 +0500

    add: AppRouter - wrap routes in page-wrapper

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -6,8 +6,12 @@ export const AppRouter = () => {
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <Routes>
-        {Object.values(ROUTE_CONFIG).map((route) => (
-          <Route {...route} key={route.path} />
+        {Object.values(ROUTE_CONFIG).map(({ path, element }) => (
+          <Route
+            path={path}
+            element={<div className="page-wrapper">{element}</div>}
+            key={path}
+          />
         ))}
       </Routes>
     </Suspense>
