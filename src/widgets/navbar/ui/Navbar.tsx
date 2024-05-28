1c20b92c2f78e5dddbda06ffae3287a98c5bce0e
Amirali <kadenov.a@mybuh.kz>
2024-05-28 18:24:43 +0500

    refactor: Navbar - add ThemeSwitcher, adjust props

--- src/widgets/navbar/ui/Navbar.tsx
+++ src/widgets/navbar/ui/Navbar.tsx
@@ -1,21 +1,26 @@
 import { FC } from "react";
 import { classNames } from "shared/lib/class-names/classNames";
-import { AppLink, AppLinkTheme } from "shared/ui/app-link/AppLink";
+import { AppLink, AppLinkVariants } from "shared/ui/app-link/ui/AppLink";
+import { ThemeSwitcher } from "features/theme-switcher";
 import cls from "./Navbar.module.scss";
 
-type Props = {
+interface Props {
   className?: string;
-};
+}
 
 export const Navbar: FC<Props> = ({ className }) => {
   return (
     <nav className={classNames(cls.Navbar, className)}>
-      <AppLink theme={AppLinkTheme.SECONDARY} to="/">
-        Главная
-      </AppLink>
-      <AppLink theme={AppLinkTheme.SECONDARY} to="/about">
-        О нас
-      </AppLink>
+      <ThemeSwitcher />
+
+      <div className={cls.links}>
+        <AppLink variant={AppLinkVariants.INVERTED} to="/">
+          Главная
+        </AppLink>
+        <AppLink variant={AppLinkVariants.INVERTED} to="/about">
+          О нас
+        </AppLink>
+      </div>
     </nav>
   );
 };
