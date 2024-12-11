494ab4675c68d9048385c0c2efbf75541ba11ebc
Amirali <kadenov.a@mybuh.kz>
2024-12-11 19:29:40 +0500

    add: setup react testing library

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -1,7 +1,18 @@
-export default {
+import type { Config } from 'jest'
+import path from 'path'
+
+const config: Config = {
   clearMocks: true,
   testEnvironment: 'jsdom',
   transformIgnorePatterns: ['\\\\node_modules\\\\'],
   rootDir: '../../',
   testMatch: ['<rootDir>src/**/*(*.)@(spec|test).[tj]s?(x)'],
+  modulePaths: ['<rootDir>/src'],
+  setupFilesAfterEnv: ['<rootDir>config/jest/setupTests.ts'],
+  moduleNameMapper: {
+    '\\.s?css$': 'identity-obj-proxy',
+    '\\.svg': path.resolve(__dirname, 'JestEmptyComponent.tsx'),
+  },
 }
+
+export default config
