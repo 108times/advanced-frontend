dd861807e25e1d84da05e3990ca496f541e150a8
Amirali <kadenov.a@mybuh.kz>
2024-12-11 19:25:38 +0500

    add: Navbar translations

--- src/widgets/navbar/ui/Navbar.tsx
+++ src/widgets/navbar/ui/Navbar.tsx
@@ -2,20 +2,24 @@ import { FC } from 'react'
 import { classNames } from 'shared/lib/class-names/classNames'
 import { AppLink, AppLinkVariants } from 'shared/ui/app-link/ui/AppLink'
 import cls from './Navbar.module.scss'
+import { useTranslation } from 'react-i18next'
 
 interface Props {
   className?: string
 }
 
-export const Navbar: FC<Props> = ({ className }) => (
-  <nav className={classNames(cls.Navbar, className)}>
-    <div className={cls.links}>
-      <AppLink variant={AppLinkVariants.INVERTED} to="/">
-        Главная
-      </AppLink>
-      <AppLink variant={AppLinkVariants.INVERTED} to="/about">
-        О нас
-      </AppLink>
-    </div>
-  </nav>
-)
+export const Navbar: FC<Props> = ({ className }) => {
+  const { t } = useTranslation()
+  return (
+    <nav className={classNames(cls.Navbar, className)}>
+      <div className={cls.links}>
+        <AppLink variant={AppLinkVariants.INVERTED} to="/">
+          {t('Главная')}
+        </AppLink>
+        <AppLink variant={AppLinkVariants.INVERTED} to="/about">
+          {t('О нас')}
+        </AppLink>
+      </div>
+    </nav>
+  )
+}
