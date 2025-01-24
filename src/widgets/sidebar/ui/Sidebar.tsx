e1bb37cdaf9b56e7d8eb4774e8133145023d676a
Amirali <kadenov.a@mybuh.kz>
2025-01-24 20:05:55 +0500

    add: Sidebar - update toggle button

--- src/widgets/sidebar/ui/Sidebar.tsx
+++ src/widgets/sidebar/ui/Sidebar.tsx
@@ -3,6 +3,7 @@ import { classNames } from 'shared/lib/class-names/classNames'
 import { ThemeSwitcher } from 'features/theme-switcher'
 import { LangSwitcher } from 'features/lang-switcher'
 import cls from './Sidebar.module.scss'
+import { Button, ButtonSizes, ButtonVariants } from 'shared/ui/button'
 
 interface Props {
   className?: string
@@ -20,13 +21,20 @@ export const Sidebar: FC<Props> = ({ className }) => {
       data-testid="sidebar"
       className={classNames(cls.Sidebar, className, collapsed && cls.collapsed)}
     >
-      <button data-testid="sidebar-toggle" onClick={toggleCollapsed}>
-        {/* toggle */}
-      </button>
+      <Button
+        data-testid="sidebar-toggle"
+        onClick={toggleCollapsed}
+        className={cls.collapsedBtn}
+        variant={ButtonVariants.BACKGROUND_INVERTED}
+        size={ButtonSizes.L}
+        square
+      >
+        {collapsed ? '>' : '<'}
+      </Button>
 
       <div className={cls.switchers}>
         <ThemeSwitcher />
-        <LangSwitcher className={cls.lang} />
+        <LangSwitcher className={cls.lang} short={collapsed} />
       </div>
     </nav>
   )
