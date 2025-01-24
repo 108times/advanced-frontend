f621bd3ba311472d261dbdd2cf35ab5fbeb587ea
Amirali <kadenov.a@mybuh.kz>
2025-01-24 20:03:16 +0500

    fix: App - add Suspense

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,16 +4,20 @@ import { Navbar } from 'widgets/navbar/'
 import { Sidebar } from 'widgets/sidebar'
 import 'shared/config/i18n/i18n'
 import { AppWrapper } from './ui/AppWrapper'
+import { Suspense } from 'react'
+import { Loader } from 'shared/ui/loader'
 
 export const App = () => {
   return (
     <AppWrapper>
-      <Navbar />
+      <Suspense fallback={<Loader />}>
+        <Navbar />
 
-      <div className="content-page">
-        <Sidebar />
-        <AppRouter />
-      </div>
+        <div className="content-page">
+          <Sidebar />
+          <AppRouter />
+        </div>
+      </Suspense>
     </AppWrapper>
   )
 }
